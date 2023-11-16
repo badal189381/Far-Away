7b6345ab53523579d2b3c7962428e4b4ba30ded3
badal189381 <1si21ec014@sit.ac.in>
2023-11-16 18:52:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Logo from "./components/logo"
+import Form from "./components/Form"
+import PackingList from "./components/PackingList"
+import Stats from "./components/Stats"
+function App() {
+
+  return <div className="app">
+  <Logo></Logo>
+  <Form></Form>
+  <PackingList></PackingList>
+  <Stats></Stats>
+  </div>
+}
+
+export default App
