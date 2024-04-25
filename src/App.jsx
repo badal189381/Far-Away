be1638d5c3114499ff42dd97ccba170a0768fd34
badal <badal@badals-MacBook-Air.local>
2024-04-26 00:47:02 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
-import Logo from "./components/logo"
-import Form from "./components/Form"
-import PackingList from "./components/PackingList"
-import Stats from "./components/Stats"
-function App() {
+import Logo from "./components/logo";
+import Form from "./components/Form";
+import PackingList from "./components/PackingList";
+import Stats from "./components/Stats";
+import { useState } from "react";
 
-  return <div className="app">
-  <Logo></Logo>
-  <Form></Form>
-  <PackingList></PackingList>
-  <Stats></Stats>
-  </div>
+function App() {
+  const [items, setItems] = useState([]);
+  function handleAddItem(item) {
+    setItems((items) => [...items, item]);
+  }
+  function handleDeleteItems(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+  return (
+    <div className="app">
+      <Logo></Logo>
+      <Form onAddItems={handleAddItem}></Form>
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItems}
+        onToggleItem={handleToggleItem}
+      ></PackingList>
+      <Stats items={items}/>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,8 +1,9 @@
-function Stats()
-{
-  return (<footer className="stats">
-   <em>You have X item on your list, and you already packed X(X%);
-   </em>
-  </footer>)
+function Stats({items}) {
+  const numsItems = items.lenght;
+  return (
+    <footer className="stats">
+      <em>You have {numsItems}on your list, and you already packed X(X%);</em>
+    </footer>
+  );
 }
 export default Stats;
